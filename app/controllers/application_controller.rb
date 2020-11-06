3930579eb1424795bd6f5ba5cf67fb4ea1af3bb8
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-06 10:46:57 +0900

    Influencers コントローラを実装して、ルーティングを記述する。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def index
-    @influencers = Influencer.all
-  end
-
-  def show
-    @influencer = Influencer.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'application#index'
+  root to: 'influencers#index'
 
-  resources :application, only: :show
+  resources :influencers, only: :show
 end
