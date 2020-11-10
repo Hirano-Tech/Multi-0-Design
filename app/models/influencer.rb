04aa2b47fd1c253cedb4eab09a203e163eb9687d
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-10 17:32:03 +0900

    Influencer モデルを作成する。

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,40 +1,4 @@
-class Influencer < ActiveHash::Base
-  self.data = [
-    {
-      id: 1,
-      name: "プチプラのあや",
-      gender: "女性",
-      header: "/assets/aya_header.png",
-    },
-
-    {
-      id: 2,
-      name: "まる",
-      gender: "女性",
-    },
-
-    {
-      id: 3,
-      name: "佐々木 あさひ",
-      gender: "女性",
-    },
-
-    {
-      id: 4,
-      name: "ななみ",
-      gender: "女性",
-    },
-
-    {
-      id: 5,
-      name: "佐藤 優里亜",
-      gender: "女性",
-    },
-
-    {
-      id: 6,
-      name: "かんだま",
-      gender: "女性",
-    }
-  ]
+class Influencer < ApplicationRecord
+  include FriendlyId
+  friendly_id :name, use: :finders
 end
