8b9d8309838eedbfb3dfbbcc0019217efd01ced9
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-10 17:33:18 +0900

    トップページで繰り返し使われている情報を変数化する。

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,9 +1,18 @@
 class InfluencersController < ApplicationController
+  before_action :url_shorter, only: [:index]
   def index
     @influencers = Influencer.all
   end
 
   def show
     @influencer = Influencer.find(params[:id])
+  end
+
+  private
+  def url_shorter
+    @influencer_icon = "https://multi-0-design.s3-ap-northeast-1.amazonaws.com/images/Influencers/"
+    @twitter = "https://twitter.com/"
+    @instagram = "https://www.instagram.com/"
+    @youtube = "https://www.youtube.com/channel/"
   end
 end
