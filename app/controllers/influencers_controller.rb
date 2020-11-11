49c266d4fbbb16cc6804938fda78208de42a1228
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-12 08:41:58 +0900

    URL を変数に纏める。

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,5 +1,5 @@
 class InfluencersController < ApplicationController
-  before_action :url_shorter, only: [:index]
+  before_action :url_shorter, only: [:index, :show]
   def index
     @influencers = Influencer.all
   end
@@ -10,9 +10,15 @@ class InfluencersController < ApplicationController
 
   private
   def url_shorter
-    @influencer_icon = "https://multi-0-design.s3-ap-northeast-1.amazonaws.com/images/Influencers/"
-    @twitter = "https://twitter.com/"
-    @instagram = "https://www.instagram.com/"
-    @youtube = "https://www.youtube.com/channel/"
+    @url_shorter = {
+      aws_images: "https://multi-0-design.s3-ap-northeast-1.amazonaws.com/images/",
+      twitter: "https://twitter.com/",
+      instagram: "https://www.instagram.com/",
+      youtube: "https://www.youtube.com/channel/",
+      uniqlo: "https://www.uniqlo.com/jp/ja/products/",
+      gu: "https://www.gu-global.com/jp/ja/products/",
+      shimamura: "https://www.shop-shimamura.com/item/",
+      fifth: "https://5-fifth.com/products/"
+    }
   end
 end
