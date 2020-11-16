30e6fa62bb89596b6066f16df5564b8e061f365a
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-17 03:04:06 +0900

    Products テーブルに Influencer カラムを追加する。

---
+++ db/migrate/20201116173824_add_influencer_to_products.rb
@@ -0,0 +1,5 @@
+class AddInfluencerToProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :products, :influencer, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_091357) do
+ActiveRecord::Schema.define(version: 2020_11_16_173824) do
 
   create_table "influencers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_11_10_091357) do
     t.string "distributor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "influencer_id"
+    t.index ["influencer_id"], name: "index_products_on_influencer_id"
   end
 
+  add_foreign_key "products", "influencers"
 end
