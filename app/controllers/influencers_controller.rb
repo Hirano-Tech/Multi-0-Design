6ffac4be662eb8acea26af3924d7fa3a34c362b4
Hirano-Tech <Information@Hirano.cfbx.jp>
2020-11-17 03:06:41 +0900

    トップページに商品一覧を追加する。

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,7 +1,7 @@
 class InfluencersController < ApplicationController
   before_action :url_shorter, only: [:index, :show]
   def index
-    @influencers = Influencer.all
+    @products = Product.page(params[:page]).per(8)
   end
 
   def show
@@ -14,11 +14,7 @@ class InfluencersController < ApplicationController
       aws_images: "https://multi-0-design.s3-ap-northeast-1.amazonaws.com/images/",
       twitter: "https://twitter.com/",
       instagram: "https://www.instagram.com/",
-      youtube: "https://www.youtube.com/channel/",
-      uniqlo: "https://www.uniqlo.com/jp/ja/products/",
-      gu: "https://www.gu-global.com/jp/ja/products/",
-      shimamura: "https://www.shop-shimamura.com/item/",
-      fifth: "https://5-fifth.com/products/"
+      youtube: "https://www.youtube.com/channel/"
     }
   end
 end
