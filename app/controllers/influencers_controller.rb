23803d752191344421ec412eefb1d2b822749235
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2020-12-07 19:57:04 +0900

    YouTube Data API からヘッダー画像とアイコン画像を取得して、データベースに保存する。

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,4 +1,9 @@
 class InfluencersController < ApplicationController
+  require 'uri'
+  require 'net/http'
+  require 'json'
+
+
   def index
     # ↓ 並び替え機能（インフルエンサー 一覧）↓
     if params[:sort] == "twitter"
@@ -42,10 +47,24 @@ class InfluencersController < ApplicationController
   end
 
   def show
-    if params[:id] == "佐藤優里亜" || params[:id] == "かんだま" || params[:id] == "水越みさと" || params[:id] == "きりまる" || params[:id] == "なぐもふうか"
-      redirect_to root_path
-    else
+    if params[:id] == "プチプラのあや" || params[:id] == "まる" || params[:id] == "佐々木あさひ" || params[:id] == "ななみ"
       @influencer = Influencer.find(params[:id])
+    else
+      redirect_to root_path
+    end
+
+    acquisition_date = (Time.now - @influencer.updated_at).divmod(24*60*60)
+    acquisition_date[0]
+    if acquisition_date[0].to_i >= 7
+      require 'google/apis/youtube_v3'
+      youtube = Google::Apis::YoutubeV3::YouTubeService.new
+      youtube.key = Rails.application.credentials.YouTube[:YouTube_Data_API]
+      options = {id: 'UCC_ElJAc9JrtkMm3zTTJ1_A'}
+      @aya_channel_banner = youtube.list_channels(:brandingSettings, options).to_h
+      @aya_channel_icon = youtube.list_channels(:snippet, options).to_h
+
+      @influencer.update(header: @aya_channel_banner[:items][0][:branding_settings][:image][:banner_external_url])
+      @influencer.update(icon: @aya_channel_icon[:items][0][:snippet][:thumbnails][:medium][:url])
     end
   end
 end
