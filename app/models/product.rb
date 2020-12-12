e8136eddcb3b50282ebe0baa4beb1752ff69270d
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2020-12-12 18:30:50 +0900

    関連付けを修正する。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :influencer, foreign_key: :influencer_id
+  has_and_belongs_to_many :youtube_videos
+  # has_many :youtube_videos, through: :youtube_products
 end
