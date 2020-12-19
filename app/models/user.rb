74847618333327d41b2d1679d80579d8664f29b5
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2020-12-20 02:55:16 +0900

    users テーブルの name カラムにデフォルト値を設定する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 8}
+
   has_many :influencer_likes
   has_many :influencers, through: :influencer_likes
 end
