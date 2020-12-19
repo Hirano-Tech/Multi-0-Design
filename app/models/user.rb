20d27cfedbe642be5c494675f24c13c559c6a390
Hirano-Tech <Information@Hirano.Mail-Box.ne.jp>
2020-12-19 19:52:58 +0900

    いいね機能で使用するモデルとの関連付けを設定する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :influencer_likes
+  has_many :influencers, through: :influencer_likes
 end
